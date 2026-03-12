eade6ae6dabc4b35c19f99fd2f13b4aff409c36a
iamdayanand <dayananddhumala@gmail.com>
2026-03-12 10:54:04 +0100

    fix: add terraform versions

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,12 +1,17 @@
 terraform {
+  required_version = ">= 1.5.0"
 
   backend "azurerm" {
-
     resource_group_name  = "tfstate-rg"
     storage_account_name = "pumpkinterraformstate"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
-
   }
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
